f3724e6dbcb4cef6554de8c982e19ef4f666a285
Vijinach <118913507+Vijinach@users.noreply.github.com>
2023-08-18 09:35:44 +0530

    script.js

--- TBApp/client/script.js
+++ TBApp/client/script.js
@@ -37,7 +37,74 @@ alert(error.message);
 }
 });
 }
-
-
-
-
+function getBookings() {
+  debugger
+    $.ajax({
+    url: "/server/TicketBookingAIO/getBookings", 
+    type: "GET",
+    success: function (Response) {
+      debugger
+      var data = getRequiredBookings(Response);
+      renderTable(data);
+      debugger
+    },
+    error: function (error) {
+      debugger
+    alert(error.message);
+    }
+  });
+  }
+  function getRequiredBookings(Response) {
+  debugger
+    var i;
+    var tabledata = [];
+    for (i = 0; i < Response.length; i++) {
+      var displayData = Response[i];
+      //var displayData = JSON.parse(Response[i]);  //local
+        var gulp = {
+            "Movie Name": displayData.MovieName,
+            "Movie ID": displayData.MovieID,
+            "Total Amount": displayData.TotalAmount,
+            "Booked Date": displayData.BookedDate,
+          }
+          debugger
+        tabledata.push(gulp);
+    }
+    debugger
+    console.log(tabledata)
+    return tabledata;
+  }
+  function renderTable(respData) {
+    debugger;
+    var col = [];
+    for (var i = 0; i < respData.length; i++) {
+        for (var key in respData[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+    var table = document.createElement("table");
+    table.classList.add("ca-table-view");
+    table.setAttribute('id', 'dataTable');
+  
+    var tr = table.insertRow(-1);
+  
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    for (var i = 0; i < respData.length; i++) {
+  
+        tr = table.insertRow(-1);
+  
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = respData[i][col[j]];
+        }
+    }
+    var divContainer = document.getElementById("ShowMovies");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+  }
